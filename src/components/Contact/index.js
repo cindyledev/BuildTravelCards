17114127bed5f1a937767b051d5fd55161edd7fc
Cindy Le <cle15@myseneca.ca>
2019-10-25 00:09:24 -0400

    fixed width issue

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -6,25 +6,27 @@ export default function Contact() {
   return (
     <div>
       <div className="App">
-        <h1>Contact Us</h1>
-        <div>
-          <form action="">
+        <div className="container">
+          <h1>Contact Us</h1>
+          <div>
+            <form action="">
 
-            <label>First Name</label>
-            <input type="text" id="fname" name="firstname" placeholder="Your first name" />
+              <label>First Name</label>
+              <input type="text" id="fname" name="firstname" placeholder="Your first name" />
 
-            <label>Last Name</label>
-            <input type="text" id="lname" name="lastname" placeholder="Your last name" />
+              <label>Last Name</label>
+              <input type="text" id="lname" name="lastname" placeholder="Your last name" />
 
-            <label>Email</label>
-            <input type="email" id="email" name="email" placeholder="Your email" />
+              <label>Email</label>
+              <input type="email" id="email" name="email" placeholder="Your email" />
 
-            <label>Subject</label>
-            <textarea id="subject" name="subject" placeholder="Write something" />
-            
-            <input type="submit" value="Submit" />
-            
-          </form>
+              <label>Subject</label>
+              <textarea id="subject" name="subject" placeholder="Write something" />
+
+              <input type="submit" value="Submit" />
+
+            </form>
+          </div>
         </div>
       </div>
     </div>
